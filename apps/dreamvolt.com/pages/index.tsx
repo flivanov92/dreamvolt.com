3e6496d661aa88ecb5055dd9db278ea2570f4ad9
Alex <flivanov92@gmail.com>
2021-11-15 21:04:33 +0000

    setup nextjs project for dreamvolt.com

---
+++ apps/dreamvolt.com/pages/index.tsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+const StyledPage = styled.div`
+  .page {
+  }
+`;
+
+export function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.styled-components file.
+   */
+  return (
+    <StyledPage>
+      <ul>
+        <li>Luni</li>
+        <li>Luni</li>
+        <li>Luni</li>
+        <li>Luni</li>
+      </ul>
+    </StyledPage>
+  );
+}
+
+export default Index;
